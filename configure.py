a94281d18aba62692a5d587b198350660bf9d729
LagoLunatic <LagoLunatic@users.noreply.github.com>
2025-05-17 16:24:36 -0400

    Update dtk, objdiff, sjiswrap, and wibo

--- configure.py
+++ configure.py
@@ -144,10 +144,10 @@ if not config.non_matching:
 # Tool versions
 config.binutils_tag = "2.42-1"
 config.compilers_tag = "20240706"
-config.dtk_tag = "v1.4.1"
-config.objdiff_tag = "v2.7.1"
-config.sjiswrap_tag = "v1.2.0"
-config.wibo_tag = "0.6.11"
+config.dtk_tag = "v1.5.1"
+config.objdiff_tag = "v3.0.0-beta.8"
+config.sjiswrap_tag = "v1.2.1"
+config.wibo_tag = "0.6.16"
 
 # Project
 config.config_path = Path("config") / config.version / "config.yml"
